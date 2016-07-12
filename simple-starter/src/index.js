c297d2642ec4070b436e3f4c9f89c6c4638e3657
koten001 <robert.g.kotenko@census.gov>
2016-07-12 17:19:36 -0400

    Added redux react starter boilerplate...again.

--- simple-starter/src/index.js
+++ simple-starter/src/index.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component.  This should produce some html
 
+// ES6 anonymous function; context for this is the enclosing context
+// when returning a single line, the braces can be eliminated and so can 'return'
+const App = () => <div>Hello there!</div>;
+
+
 // Attach the component to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
