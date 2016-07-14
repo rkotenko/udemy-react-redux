457c339567067dffe88cd79bd5845c415c25db33
koten001 <robert.g.kotenko@census.gov>
2016-07-14 09:30:19 -0400

    Completed 1-15: exports

--- simple-starter/src/index.js
+++ simple-starter/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
+
+const API_KEY = ' AIzaSyBFFnTgaaWf85XRXeUpA501Yk7QJVYnYPg ';
 
 // Create a new component.  This should produce some html
 
 // ES6 anonymous function; context for this is the enclosing context
 // when returning a single line, the braces can be eliminated and so can 'return'
-const App = () => <div>Hello there!</div>;
+//const App = () => <div>Hello there!</div>;
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 
 // Attach the component to the DOM
