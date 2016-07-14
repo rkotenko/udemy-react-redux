3353631cf0448909114611a68cd44beae30d91a1
koten001 <robert.g.kotenko@census.gov>
2016-07-14 18:17:16 -0400

    middle of 2-24: building lists with maps

---
+++ simple-starter/src/components/video-list.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import VideoListItem from './video-list-item';
+const VideoList = (props) => {
+  return (
+    <ul className="col-md-4 list-group">
+
+    </ul>
+  );
+};
+
+export default VideoList;
--- simple-starter/src/index.js
+++ simple-starter/src/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
+import VideoList from'./components/video-list';
 
 const API_KEY = ' AIzaSyBFFnTgaaWf85XRXeUpA501Yk7QJVYnYPg ';
 
-// Create a new component.  This should produce some html
+
 
 // ES6 anonymous function; context for this is the enclosing context
 // when returning a single line, the braces can be eliminated and so can 'return'
 //const App = () => <div>Hello there!</div>;
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });  // ES6 allows using just the name of the property when key/property have same name
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 };
 
 
