d86e8a3b4a97cf02a44618156663f7a3db2dbf68
koten001 <robert.g.kotenko@census.gov>
2016-07-14 13:59:22 -0400

    Completed 1-19: Controlled Components

--- simple-starter/src/components/search-bar.js
+++ simple-starter/src/components/search-bar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+// imports React as well as Component from within react as its own variable.
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input placeholder="Search"/>
-};
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    //initial state
+    this.state = {term: ''};
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value = {this.state.term} // setting value by state makes this a "controlled" component and its value can only be changed through change in state
+          onChange={e => this.setState({term: e.target.value})}
+        />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+}
 
 export default SearchBar;
