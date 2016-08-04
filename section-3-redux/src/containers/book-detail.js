2717c8330a011069a89d4bc5d0a03a9263ecfda9
koten001 <robert.g.kotenko@census.gov>
2016-08-04 12:43:13 -0400

    Finished the book list redux app.

--- section-3-redux/src/containers/book-detail.js
+++ section-3-redux/src/containers/book-detail.js
@@ -3,8 +3,15 @@ import {connect} from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if(!this.props.book) {
+      return <div>Select a book to get details</div>;
+    }
+
     return (
-      <div>book details</div>
+      <div>
+      <h3>{this.props.book.title}</h3>
+      <div>page count: {this.props.book.pages}</div>
+      </div>
     );
   }
 }
--- section-3-redux/src/reducers/reducer-books.js
+++ section-3-redux/src/reducers/reducer-books.js
@@ -1,9 +1,9 @@
 export default function BooksReducer() {
   return [
-    {title: 'Great Things'},
-    {title: "Less Things"},
-    {title: "A story about a boy"},
-    {title: "Vampires!"},
-    {title: "Dogs and cats"}
+    {title: 'Great Things', pages: 100},
+    {title: "Less Things", pages: 350},
+    {title: "A story about a boy", pages: 9},
+    {title: "Vampires!", pages: 6},
+    {title: "Dogs and cats", pages: 15}
   ];
 };
