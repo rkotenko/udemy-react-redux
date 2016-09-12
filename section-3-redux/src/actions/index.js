67c245e5b5ac2dc9393449d743c2772a2a48a1dd
rkotenko <robkotenko@gmail.com>
2016-09-12 15:06:59 -0400

    restarting section 3 because I forgot too much

--- section-3-redux/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export function selectBook(book) {
-  // selectBook is an ActionCreator so needs to return an action, which is an object with a type property
-  return {
-    type: 'BOOK_SELECTED',
-    payload: book
-  }
-}
-
-
